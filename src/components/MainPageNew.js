a25c3ab08f36e42d1a5fce01d9f3733920e25ce4
Your Name <eshanjindal9@gmail.com>
2019-09-09 06:33:45 +0530

    Adding Count

--- src/components/MainPageNew.js
+++ src/components/MainPageNew.js
@@ -10,17 +10,43 @@ export class MainPageNew extends Component {
     
         this.state = {
             questions: [
-                {
-                    content: '',
-                    options: []
-                }
+                // {
+                //     content: '',
+                //     options: []
+                // }
             ],
             loading: false,
             currentPage: 1,
-            questionsPerPage: 1
+            questionsPerPage: 1,
+            selectedOption: []
         }
+
+        this.handleOptionChange = this.handleOptionChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
+
+    handleOptionChange(e) {
+        
+        this.setState({
+            selectedOption: [...this.state.selectedOption, e.target.value]
+        })
+    
+        console.log('Event Selected', e)
+    }
+
+    handleSubmit(e) {
+      
+         var count = 0
+       for(var i = 0; i < this.state.selectedOption.length; i++) {
+            if(this.state.selectedOption[i] === this.state.questions[i].answer) {           
+                 count = count + 1
+            }
+       }
+
+       console.log('Your Score is', count)
+         e.preventDefault()
+    }
     componentDidMount() {
         this.setState({loading: true})
         axios.get("http://localhost:3002/test")
@@ -28,10 +54,10 @@ export class MainPageNew extends Component {
             console.log('Response data for questions', response)
             for(var i = 0; i < response.data.length; i++){
                 
-                this.setState({
-                    questions: [{content: response.data[i].content, options: response.data[i].options}, ...this.state.questions]
+                this.setState(prevState => ({
+                    questions: [...prevState.questions, {content: response.data[i].content, options: response.data[i].options, answer: response.data[i].answer.answer_value} ]
                        
-                 })
+                }))
             }
 
             this.setState({loading: false})
@@ -52,11 +78,13 @@ export class MainPageNew extends Component {
 
     const paginate = pageNumber => this.setState({currentPage: pageNumber})
         return (
+            <form onSubmit={this.handleSubmit}>
             <div>
-                <Questions questions={currentQuestions} loading={this.state.loading} />
+                <Questions questions={currentQuestions} loading={this.state.loading} handleOptionChange={this.handleOptionChange} selectedOption={this.state.selectedOption}/>
             <Pagination questionsPerPage={this.state.questionsPerPage} totalQuestions={this.state.questions.length} paginate={paginate}/>
               <button>Submit</button>
             </div>
+            </form>
         )
     }
 }
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,19 +1,24 @@
 import React from 'react'
 
 
-const Questions = ({ questions, loading}) => {
+const Questions = ({ questions, loading, selectedOption, handleOptionChange}) => {
 
+    
     if(loading) {
         return <h2>Loading.....</h2>
     }
 
+    
 
     return (
         <div>
           
             {questions.map(question => <div key={question.content}>{question.content}
                             <div className = "radio">{question.options.map(option => 
-                             <p key={option.value}><input type = "radio"/>
+                             <p key={option.value}><input type = "radio" name = "options"
+                             value = {option.value}
+                             checked = {selectedOption === option.value}
+                             onChange = {handleOptionChange}/>
                               {option.value}
                              </p>
             )}</div></div>)}
